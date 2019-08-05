c571f886e0801b750131c8579c51718f264e22cc
Rushani Shawuti <ruhsane924@gmail.com>
2019-08-05 15:54:41 -0700

    files added

---
+++ challenge_4/challenge_4.py
@@ -0,0 +1,12 @@
+def knapsack(C, items, n):
+  ''' A  method to determine the maximum value of the items included in the knapsack 
+without exceeding the capacity  C
+
+    Parameters: 
+    C= 50
+    items = (("boot", 10, 60),
+         ("tent", 20, 100),
+         ("water", 30, 120),
+         ("first aid", 15, 70))
+    Returns: max value
+'''
