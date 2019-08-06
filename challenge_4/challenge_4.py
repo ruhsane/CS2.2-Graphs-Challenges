6d2057ea19b376cdefbf2aac071aff6aea620291
Rushani Shawuti <ruhsane924@gmail.com>
2019-08-05 21:34:19 -0700

    added comments

--- challenge_4/challenge_4.py
+++ challenge_4/challenge_4.py
@@ -23,18 +23,26 @@ def knapsack(capacity, items):
 
     item = items[0]
 
+    # if item weights more than the available capacity, ignore the item, call recursive function again
     if item.weight > capacity:
         return knapsack(capacity, items[1:])
 
+    # if item weight is fittable,
+
+    # first calculate the value of rest of the item including this current item, until it reaches capacity
     value_with, items_used_with = knapsack(capacity - item.weight, items[1:])
     value_with += item.value
     
+    # second calculate the value of rest of the items, excluding the current item
     value_without, items_used_without = knapsack(capacity, items[1:])
 
+    # check which calculation has higher value
     if value_with >= value_without:
+        # if the value is higher including the item, use this item
         items_used_with.append(item.name)
         return (value_with, items_used_with)
 
+    # if the value is higher without the item, ignore this item, call this function again 
     return (value_without, items_used_without)
 
 items = (Item("boot", 10, 60),
