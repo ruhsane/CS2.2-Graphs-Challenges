c5f12f29fdb307065a84ac681184db1b9b980c58
Rushani Shawuti <ruhsane924@gmail.com>
2019-08-05 17:09:40 -0700

    knapsack completed

--- challenge_4/challenge_4.py
+++ challenge_4/challenge_4.py
@@ -1,12 +1,45 @@
-def knapsack(C, items, n):
-  ''' A  method to determine the maximum value of the items included in the knapsack 
-without exceeding the capacity  C
+class Item():
+    def __init__(self, name, weight, value):
+        self.name = name
+        self.weight = weight
+        self.value = value
 
-    Parameters: 
-    C= 50
-    items = (("boot", 10, 60),
-         ("tent", 20, 100),
-         ("water", 30, 120),
-         ("first aid", 15, 70))
-    Returns: max value
-'''
+def knapsack(capacity, items):
+    ''' A  method to determine the maximum value of the items included in the knapsack 
+    without exceeding the capacity  C
+
+        Parameters: 
+        C= 50
+        items = (("boot", 10, 60),
+            ("tent", 20, 100),
+            ("water", 30, 120),
+            ("first aid", 15, 70))
+        Returns: max value
+    '''
+
+    #base case
+    if len(items) == 0 or capacity == 0:
+        return (0, [])
+
+    item = items[0]
+
+    if item.weight > capacity:
+        return knapsack(capacity, items[1:])
+
+    value_with, items_used_with = knapsack(capacity - item.weight, items[1:])
+    value_with += item.value
+    
+    value_without, items_used_without = knapsack(capacity, items[1:])
+
+    if value_with >= value_without:
+        items_used_with.append(item.name)
+        return (value_with, items_used_with)
+
+    return (value_without, items_used_without)
+
+items = (Item("boot", 10, 60),
+        Item("tent", 20, 100),
+        Item("water", 30, 120),
+        Item("first aid", 15, 70))
+
+print(knapsack(50, items))
