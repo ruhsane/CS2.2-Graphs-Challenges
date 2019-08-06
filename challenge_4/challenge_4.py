ca8018e155943f7df886799d29f288d1eec0a4b1
Rushani Shawuti <ruhsane924@gmail.com>
2019-08-05 21:58:00 -0700

    formatted the output

--- challenge_4/challenge_4.py
+++ challenge_4/challenge_4.py
@@ -45,9 +45,26 @@ def knapsack(capacity, items):
     # if the value is higher without the item, ignore this item, call this function again 
     return (value_without, items_used_without)
 
-items = (Item("boot", 10, 60),
-        Item("tent", 20, 100),
-        Item("water", 30, 120),
-        Item("first aid", 15, 70))
+def ouput(items, capacity):
 
-print(knapsack(50, items))
+    #turn each item into objects with necessary properties
+    item_objects = []
+    for item in items:
+        name = item[0]
+        weight = item[1]
+        value = item[2]
+        item_objects.append(Item(name,weight,value))
+
+    #call the main function
+    value,used_items = knapsack(capacity, item_objects)
+    #ouput
+    print("For this input: items = %s. Capacity of knapsack = %s. \n The value of the optimal solution to the knapsack problem is V= %s. \n The items included in the knapsack for this optimal solution are: %s" % (items, capacity, value, used_items))
+
+
+if __name__ == "__main__":
+    items = (("boot", 10, 60),
+            ("tent", 20, 100),
+            ("water", 30, 120),
+            ("first aid", 15, 70))
+
+    ouput(items, 50)
