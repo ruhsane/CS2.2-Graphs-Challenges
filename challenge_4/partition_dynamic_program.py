ccced8a0fcacbb18d826e9d22fca4e86efa4645f
Rushani Shawuti <ruhsane924@gmail.com>
2019-08-11 04:14:14 -0700

    dynamic solution with memoization

--- challenge_4/partition_dynamic_program.py
+++ challenge_4/partition_dynamic_program.py
@@ -5,66 +5,30 @@ For this input:
 The optimal solution is:
     YYYY
 
-The [values] included for this optimal solution are: 
-    ZZZZ
 '''
+def canPartitionKSubsets(nums, k):
+    target, rem = divmod(sum(nums), k)
+    if rem or max(nums) > target: return False
 
-'''
-Partition problem is to determine whether a given set can be partitioned into two subsets such that the sum of elements in both subsets is same.
-Examples:
-
-arr[] = {1, 5, 11, 5}
-Output: true 
-The array can be partitioned as {1, 5, 5} and {11}
-
-arr[] = {1, 5, 3}
-Output: false 
-The array cannot be partitioned into equal sum sets.
-
-Following are the two main steps to solve this problem:
-1) Calculate sum of the array. If sum is odd, there can not be two subsets with equal sum, so return false.
-2) If sum of array elements is even, calculate sum/2 and find a subset of array with sum equal to sum/2.
-
-'''
-
-'''
-Let isSubsetSum(arr, n, sum/2) be the function that returns true if 
-there is a subset of arr[0..n-1] with sum equal to sum/2
-'''
-
-def isSubsetSum(arr, n, sum):
-    print(arr)
-    print(sum)
-    #base case
-    if sum == 0:
-        return True
-
-    if n == 0 and sum != 0:
-        return False
-
-    if arr[n-1] > sum: 
-        return isSubsetSum (arr, n-1, sum) 
-
-    with_last_element = isSubsetSum(arr, n-1 , sum)
-    without_last_element = isSubsetSum(arr, n-1, sum - arr[n-1])
-
-    # return whichever is true
-    return with_last_element or without_last_element
-
-
-def partition(arr, length):
+    memo = [None] * (1 << len(nums))
+    memo[-1] = True
     
-    sum = 0
+    def search(used, todo):
+        if memo[used] is None:
+            targ = (todo - 1) % target + 1
+            memo[used] = any(search(used | (1<<i), todo - num)
+                                for i, num in enumerate(nums)
+                                if (used >> i) & 1 == 0 and num <= targ)
 
-    for i in arr:
-        sum += i 
+        return memo[used]
 
-    if sum % 2 != 0:
-        return False
-    
-    return isSubsetSum(arr, length, sum/2)
+    return search(0, target * k)
+
 
 if __name__ == "__main__":
     arr = [1, 5, 11, 5]
-    length = len(arr)
-    print(partition(arr, length))
+    print("For this input: [1, 5, 11, 5], ouput is: ")
+    print(canPartitionKSubsets(arr, 2))
+    arr2 = [1, 5, 3]  
+    print("For this input: [1, 5, 3], ouput is: ")
+    print(canPartitionKSubsets(arr2, 2))
